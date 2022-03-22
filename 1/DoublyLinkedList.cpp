29af36207663b20915156901f83637b4df8720cc
naolchala <naolchala6@gmail.com>
2022-03-22 23:39:24 +0300

    error handling

--- 1/DoublyLinkedList.cpp
+++ 1/DoublyLinkedList.cpp
@@ -1,164 +1,164 @@
 #include <iostream>
-#include <stdexcept>
 using namespace std;
 
 class Node
 {
 public:
-  int data;
-  Node *next = NULL;
-  Node *prev = NULL;
+	int data;
+	Node *next = NULL;
+	Node *prev = NULL;
 
-  Node(int d) { data = d; }
+	Node(int d) { data = d; }
 };
 
 class DoublyLinkedList
 {
 public:
-  Node *head = NULL;
-  int length = 0;
+	Node *head = NULL;
+	int length = 0;
 
-  void print();
+	void print();
 
-  void addAtFirst(int data);
-  void addAtIndex(int data, int index);
-  void addAtLast(int data);
+	void addAtFirst(int data);
+	void addAtIndex(int data, int index);
+	void addAtLast(int data);
 
-  void removeFirst();
-  void removeAtIndex(int index);
-  void removeLast();
+	void removeFirst();
+	void removeAtIndex(int index);
+	void removeLast();
 };
 
 void DoublyLinkedList::print()
 {
-  Node *temp = head;
-  while (temp != NULL)
-  {
-    cout << temp->data;
-    temp = temp->next;
-    if (temp != NULL)
-    {
-      cout << " <-> ";
-    }
-  }
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data;
+		temp = temp->next;
+		if (temp != NULL)
+		{
+			cout << " <-> ";
+		}
+	}
 
-  cout << endl;
+	cout << endl;
 }
 void DoublyLinkedList::addAtFirst(int data)
 {
-  Node *newNode = new Node(data);
-  Node *temp = head;
-  newNode->next = temp;
-  if (temp != NULL)
-    temp->prev = newNode;
-  head = newNode;
-  length++;
+	Node *newNode = new Node(data);
+	Node *temp = head;
+	newNode->next = temp;
+	if (temp != NULL)
+		temp->prev = newNode;
+	head = newNode;
+	length++;
 };
 void DoublyLinkedList::addAtIndex(int data, int index)
 {
-  if (index == 0)
-  {
-    addAtFirst(data);
-    return;
-  }
-  else if (index > length)
-  {
-    throw out_of_range("Index Out of Bounds");
-    return;
-  }
+	if (index == 0)
+	{
+		addAtFirst(data);
+		return;
+	}
+	else if (index > length)
+	{
+		cerr << "Index Out of Bounds" << endl;
+		exit(-1);
+	}
 
-  int i = 0;
-  Node *temp = head;
+	int i = 0;
+	Node *temp = head;
 
-  while (temp != NULL && i < index - 1)
-  {
-    temp = temp->next;
-    i++;
-  }
+	while (temp != NULL && i < index - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
 
-  Node *newNode = new Node(data);
-  Node *next = temp->next;
-  next->prev = newNode;
-  newNode->next = next;
-  newNode->prev = temp;
-  temp->next = newNode;
-  length++;
+	Node *newNode = new Node(data);
+	Node *next = temp->next;
+	next->prev = newNode;
+	newNode->next = next;
+	newNode->prev = temp;
+	temp->next = newNode;
+	length++;
 };
 
 void DoublyLinkedList::addAtLast(int data)
 {
-  Node *newNode = new Node(data);
-  Node *temp = head;
+	Node *newNode = new Node(data);
+	Node *temp = head;
 
-  while (temp->next != NULL)
-    temp = temp->next;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-  temp->next = newNode;
-  newNode->prev = temp;
-  length++;
+	temp->next = newNode;
+	newNode->prev = temp;
+	length++;
 };
 
 void DoublyLinkedList::removeFirst()
 {
-  head = head->next;
-  head->prev = NULL;
-  length--;
+	head = head->next;
+	head->prev = NULL;
+	length--;
 }
 void DoublyLinkedList::removeAtIndex(int index)
 {
-  if (index == 0)
-    removeFirst();
-  else if (index >= length - 1)
-  {
-    throw out_of_range("Index Out Of Bounds");
-    return;
-  }
-  else
-  {
-    int i = 0;
-    Node *temp = head;
-    while (temp != NULL && i < index - 1)
-    {
-      temp = temp->next;
-      i++;
-    }
-    temp->next = temp->next->next;
-    temp->next->prev = temp;
-    length--;
-  }
+	if (index == 0)
+		removeFirst();
+	else if (index >= length - 1)
+	{
+		cerr << "Index Out of Bounds " << endl;
+		exit(-1);
+		return;
+	}
+	else
+	{
+		int i = 0;
+		Node *temp = head;
+		while (temp != NULL && i < index - 1)
+		{
+			temp = temp->next;
+			i++;
+		}
+		temp->next = temp->next->next;
+		temp->next->prev = temp;
+		length--;
+	}
 }
 
 void DoublyLinkedList::removeLast()
 {
-  if (head != NULL)
-  {
-    Node *temp = head;
-    while (temp->next->next != NULL)
-      temp = temp->next;
-    temp->next = NULL;
-    length--;
-  }
+	if (head != NULL)
+	{
+		Node *temp = head;
+		while (temp->next->next != NULL)
+			temp = temp->next;
+		temp->next = NULL;
+		length--;
+	}
 }
 
 int main()
 {
-  DoublyLinkedList dll;
-  cout << "Adding" << endl;
-  dll.addAtFirst(2);
-  dll.addAtFirst(3);
-  dll.addAtFirst(4);
-  dll.addAtIndex(5, 2);
-  dll.addAtLast(6);
-  cout << "List Size: " << dll.length << endl;
-  dll.print();
+	DoublyLinkedList dll;
+	cout << "Adding" << endl;
+	dll.addAtFirst(2);
+	dll.addAtFirst(3);
+	dll.addAtFirst(4);
+	dll.addAtIndex(5, 2);
+	dll.addAtLast(6);
+	cout << "List Size: " << dll.length << endl;
+	dll.print();
 
-  cout << endl
-       << "Deleting " << endl;
-  dll.removeLast();
-  dll.removeFirst();
-  dll.removeAtIndex(1);
-  cout << "List Size: " << dll.length << endl;
-  dll.print();
+	cout << endl
+		 << "Deleting " << endl;
+	dll.removeLast();
+	dll.removeFirst();
+	dll.removeAtIndex(1);
+	cout << "List Size: " << dll.length << endl;
+	dll.print();
 
-  return 0;
+	return 0;
 }
--- 1/SinglyLinkedList.cpp
+++ 1/SinglyLinkedList.cpp
@@ -1,127 +1,148 @@
 #include <iostream>
-#include <stdexcept>
 using namespace std;
 
-class Node {
- public:
-  int data;
-  Node *next = NULL;
+class Node
+{
+public:
+	int data;
+	Node *next = NULL;
 
-  Node(int d) { data = d; }
+	Node(int d) { data = d; }
 };
 
-class SinglyLinkedList {
- public:
-  Node *head = NULL;
-  int length = 0;
+class SinglyLinkedList
+{
+public:
+	Node *head = NULL;
+	int length = 0;
 
-  void print();
+	void print();
 
-  void addAtFirst(int data);
-  void addAtIndex(int data, int index);
-  void addAtLast(int data);
+	void addAtFirst(int data);
+	void addAtIndex(int data, int index);
+	void addAtLast(int data);
 
-  void removeFirst();
-  void removeAtIndex(int index);
-  void removeLast();
+	void removeFirst();
+	void removeAtIndex(int index);
+	void removeLast();
 };
 
-void SinglyLinkedList::print() {
-  Node *temp = head;
-  while (temp != NULL) {
-    cout << temp->data;
-    temp = temp->next;
-    if (temp != NULL) {
-      cout << " -> ";
-    }
-  }
+void SinglyLinkedList::print()
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data;
+		temp = temp->next;
+		if (temp != NULL)
+		{
+			cout << " -> ";
+		}
+	}
 
-  cout << endl;
+	cout << endl;
 }
-void SinglyLinkedList::addAtFirst(int data) {
-  Node *newNode = new Node(data);
-  Node *temp = head;
-  newNode->next = temp;
-  head = newNode;
-  length++;
+void SinglyLinkedList::addAtFirst(int data)
+{
+	Node *newNode = new Node(data);
+	Node *temp = head;
+	newNode->next = temp;
+	head = newNode;
+	length++;
 };
-void SinglyLinkedList::addAtIndex(int data, int index) {
-  if (index == 0) {
-    addAtFirst(data);
-    return;
-  } else if (index > length) {
-    throw out_of_range("Index Out of Bounds");
-    return;
-  }
+void SinglyLinkedList::addAtIndex(int data, int index)
+{
+	if (index == 0)
+	{
+		addAtFirst(data);
+		return;
+	}
+	else if (index > length)
+	{
+		cerr << "Index Out of Bounds" << endl;
+		exit(-1);
+	}
 
-  int i = 0;
-  Node *temp = head;
+	int i = 0;
+	Node *temp = head;
 
-  while (temp != NULL && i < index - 1) {
-    temp = temp->next;
-    i++;
-  }
+	while (temp != NULL && i < index - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
 
-  Node *newNode = new Node(data);
-  Node *next = temp->next;
-  newNode->next = next;
-  temp->next = newNode;
-  length++;
+	Node *newNode = new Node(data);
+	Node *next = temp->next;
+	newNode->next = next;
+	temp->next = newNode;
+	length++;
 };
-void SinglyLinkedList::addAtLast(int data) {
-  Node *newNode = new Node(data);
-  Node *temp = head;
+void SinglyLinkedList::addAtLast(int data)
+{
+	Node *newNode = new Node(data);
+	Node *temp = head;
 
-  while (temp->next != NULL) temp = temp->next;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-  temp->next = newNode;
-  length++;
+	temp->next = newNode;
+	length++;
 };
 
-void SinglyLinkedList::removeFirst() {
-  head = head->next;
-  length--;
+void SinglyLinkedList::removeFirst()
+{
+	head = head->next;
+	length--;
 }
-void SinglyLinkedList::removeAtIndex(int index) {
-  if (index == 0)
-    removeFirst();
-  else if (index > length - 1) {
-    throw out_of_range("Index Out Of Bounds");
-    return;
-  } else {
-    int i = 0;
-    Node *temp = head;
-    while (temp != NULL && i < index - 1) {
-      temp = temp->next;
-      i++;
-    }
-    temp->next = temp->next->next;
-    length--;
-  }
+void SinglyLinkedList::removeAtIndex(int index)
+{
+	if (index == 0)
+		removeFirst();
+	else if (index > length - 1)
+	{
+		cerr << "Index Out of Bounds" << endl;
+		exit(-1);
+	}
+	else
+	{
+		int i = 0;
+		Node *temp = head;
+		while (temp != NULL && i < index - 1)
+		{
+			temp = temp->next;
+			i++;
+		}
+		temp->next = temp->next->next;
+		length--;
+	}
 }
-void SinglyLinkedList::removeLast() {
-  Node *temp = head;
-  while (temp->next->next != NULL) temp = temp->next;
-  temp->next = NULL;
-  length--;
+void SinglyLinkedList::removeLast()
+{
+	Node *temp = head;
+	while (temp->next->next != NULL)
+		temp = temp->next;
+	temp->next = NULL;
+	length--;
 }
 
-int main() {
-  SinglyLinkedList sll;
+int main()
+{
+	SinglyLinkedList sll;
 
-  cout << "Adding" << endl;
-  sll.addAtFirst(2);
-  sll.addAtFirst(3);
-  sll.addAtFirst(4);
-  sll.addAtIndex(5, 3);
-  sll.addAtLast(6);
-  sll.print();
+	cout << "Adding" << endl;
+	sll.addAtFirst(2);
+	sll.addAtFirst(3);
+	sll.addAtFirst(4);
+	sll.addAtIndex(5, 3);
+	sll.addAtLast(6);
+	sll.print();
 
-  cout << "Deleting " << endl;
-  sll.removeLast();
-  sll.removeFirst();
-  sll.removeAtIndex(1);
-  sll.print();
+	cout << "Deleting " << endl;
+	sll.removeLast();
+	sll.removeFirst();
+	sll.removeAtIndex(1);
+	sll.print();
 
-  return 0;
+	return 0;
 }
